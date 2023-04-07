c00765f4537fb7d0ac70fdead863a236309365d6
syamozipc <syamozipc@gmail.com>
2023-04-07 09:25:50 +0900

    initial commit

---
+++ src/components/repositories/RepositoriesListItem.test.js
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import RepositoriesListItem from "./RepositoriesListItem";
+
+function renderComponent() {
+  const repository = {
+    full_name: "facebook/react",
+    language: "Javascript",
+    description: "a js library",
+    owner: "faceook",
+    name: "react",
+    html_url: "https://github.com/facebook/react",
+  };
+  render(
+    <MemoryRouter>
+      <RepositoriesListItem repository={repository} />
+    </MemoryRouter>
+  );
+}
+
+test("必要コンテンツが表示されているか", async () => {
+  renderComponent();
+
+  await screen.findByRole("img", { name: "Javascript" });
+});
---
+++ src/components/repositories/RepositoriesSummary.test.js
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import RepositoriesSummary from "./RepositoriesSummary";
+
+test("必要コンテンツが表示されているか", () => {
+  const repository = {
+    stargazers_count: 5,
+    open_issues: 1,
+    forks: 2,
+    language: "JavaScript",
+  };
+
+  render(<RepositoriesSummary repository={repository} />);
+
+  for (const key in repository) {
+    const value = repository[key];
+
+    const element = screen.getByText(new RegExp(value));
+
+    expect(element).toBeInTheDocument();
+  }
+});
