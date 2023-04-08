ed0f2ae14827cd7bafbb11c6e5c1db3a25897f0f
syamozipc <syamozipc@gmail.com>
2023-04-08 16:34:06 +0900

    update

--- src/routes/HomeRoute.test.js
+++ src/routes/HomeRoute.test.js
@@ -7,14 +7,13 @@ import HomeRoute from './HomeRoute';
 const handlers = [
   // クエリストリングはAPIパスに書けない
   rest.get('/api/repositories', (req, res, ctx) => {
-    const query = req.url.searchParams('q');
-    console.log(query);
+    const language = req.url.searchParams.get('q').split(':').at(-1);
 
     return res(
       ctx.json({
         items: [
-          { id: 1, full_name: 'hogehoge' },
-          { id: 2, full_name: 'fugafuga' },
+          { id: 1, full_name: `${language}_one` },
+          { id: 2, full_name: `${language}_two` },
         ],
       })
     );
@@ -31,4 +30,33 @@ afterEach(() => {
 });
 afterAll(() => {
   server.close();
+});
+
+test('2つのリポジトリが表示されていること', async () => {
+  render(
+    <MemoryRouter>
+      <HomeRoute />
+    </MemoryRouter>
+  );
+
+  const languages = [
+    'javascript',
+    'typescript',
+    'rust',
+    'go',
+    'python',
+    'java',
+  ];
+
+  for (const language of languages) {
+    const links = await screen.findAllByRole('link', {
+      name: new RegExp(`${language}_`),
+    });
+
+    expect(links).toHaveLength(2);
+    expect(links[0]).toHaveTextContent(`${language}_one`);
+    expect(links[0]).toHaveAttribute('href', `/repositories/${language}_one`);
+    expect(links[1]).toHaveTextContent(`${language}_two`);
+    expect(links[1]).toHaveAttribute('href', `/repositories/${language}_two`);
+  }
 });
