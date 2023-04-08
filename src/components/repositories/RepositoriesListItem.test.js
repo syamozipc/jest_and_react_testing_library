4693dc581f1367b5cc00a500185b4b1a39e9194b
syamozipc <syamozipc@gmail.com>
2023-04-08 11:35:10 +0900

    update

--- src/components/repositories/RepositoriesListItem.test.js
+++ src/components/repositories/RepositoriesListItem.test.js
@@ -2,18 +2,18 @@ import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import RepositoriesListItem from "./RepositoriesListItem";
 
-jest.mock("../tree/FileIcon", () => {
-  return () => {
-    return "File Icon Component";
-  };
-});
+// jest.mock("../tree/FileIcon", () => {
+//   return () => {
+//     return "File Icon Component";
+//   };
+// });
 
 function renderComponent() {
   const repository = {
     full_name: "facebook/react",
     language: "Javascript",
     description: "a js library",
-    owner: "faceook",
+    owner: { login: "facebook" },
     name: "react",
     html_url: "https://github.com/facebook/react",
   };
@@ -22,10 +22,36 @@ function renderComponent() {
       <RepositoriesListItem repository={repository} />
     </MemoryRouter>
   );
+
+  return { repository };
 }
 
 test("必要コンテンツが表示されているか", async () => {
-  renderComponent();
+  const { repository } = renderComponent();
+
   //FileIconをmockする場合はFileIconの描画待ち不要
-  // await screen.findByRole("img", { name: "Javascript" });
+  await screen.findByRole("img", { name: "Javascript" });
+
+  const link = screen.getByRole("link", { name: /github repository/i });
+  expect(link).toHaveAttribute("href", repository.html_url);
+});
+
+test("正しい言語アイコンが表示されているか", async () => {
+  renderComponent();
+
+  const icon = await screen.findByRole("img", { name: "Javascript" });
+  expect(icon).toHaveClass("js-icon");
+});
+
+test("エディタページへのリンクが正しいか", async () => {
+  const { repository } = renderComponent();
+
+  await screen.findByRole("img", { name: "Javascript" });
+
+  const link = await screen.findByRole("link", {
+    name: new RegExp(repository.owner.login),
+  });
+
+  screen.debug(link);
+  expect(link).toHaveAttribute("href", `/repositories/${repository.full_name}`);
 });
