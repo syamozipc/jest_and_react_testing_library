6679d5b40ad4f8cca01e2d0bf742a9d8488b8131
syamozipc <syamozipc@gmail.com>
2023-04-11 23:04:18 +0900

    update

--- src/components/auth/AuthButton.test.js
+++ src/components/auth/AuthButton.test.js
@@ -2,12 +2,17 @@ import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { createServer } from '../../test/server';
 import AuthButtons from './AuthButtons';
+import { SWRConfig } from 'swr';
 
 async function renderComponent() {
   render(
-    <MemoryRouter>
-      <AuthButtons />
-    </MemoryRouter>
+    // SWRのキャッシュが効いてしまいテストが正常に動作しない問題を避けるためにSWRConfigで囲う必要がある
+    // https://swr.vercel.app/ja/docs/advanced/cache#%E3%83%86%E3%82%B9%E3%83%88%E3%82%B1%E3%83%BC%E3%82%B9%E9%96%93%E3%81%AE%E3%82%AD%E3%83%A3%E3%83%83%E3%82%B7%E3%83%A5%E3%81%AE%E3%83%AA%E3%82%BB%E3%83%83%E3%83%88
+    <SWRConfig value={{ provider: () => new Map() }}>
+      <MemoryRouter>
+        <AuthButtons />
+      </MemoryRouter>
+    </SWRConfig>
   );
   await screen.findAllByRole('link');
 }
@@ -23,6 +28,7 @@ describe('when user is not signed in', () => {
   ]);
 
   test('sign in and sign up are visible', async () => {
+    // debugger;
     await renderComponent();
 
     const signInBtn = screen.getByRole('link', { name: /Sign In/i });
@@ -33,7 +39,7 @@ describe('when user is not signed in', () => {
     expect(signUpBtn).toBeInTheDocument();
     expect(signUpBtn).toHaveAttribute('href', '/signup');
   });
-  test.only('sign out is not visible', async () => {
+  test('sign out is not visible', async () => {
     await renderComponent();
 
     const signOutBtn = screen.queryByRole('link', { name: /Sign Out/i });
@@ -42,7 +48,7 @@ describe('when user is not signed in', () => {
   });
 });
 
-describe.only('when user is signed in', () => {
+describe('when user is signed in', () => {
   createServer([
     {
       path: '/api/user',
@@ -52,7 +58,8 @@ describe.only('when user is signed in', () => {
     },
   ]);
 
-  test.only('sign in and sign up are not visible', async () => {
+  test('sign in and sign up are not visible', async () => {
+    // debugger;
     await renderComponent();
 
     const signInBtn = screen.queryByRole('link', { name: /Sign In/i });
@@ -62,7 +69,7 @@ describe.only('when user is signed in', () => {
     expect(signUpBtn).not.toBeInTheDocument();
   });
   test('sign out is visible', async () => {
-    renderComponent();
+    await renderComponent();
 
     const signOutBtn = screen.getByRole('link', { name: /Sign Out/i });
 
